2885fcd18cdac5e7766630d1c9603afc7dac974b
wasya1212 <leonardo.1212@yandex.ua>
2018-10-21 00:54:40 +0300

    fixed for heroku

---
+++ client/build/precache-manifest.f2fd102edee9105fbb1d1eaa9bf72b03.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "439e34a1fb57f13a7474",
+    "url": "/static/js/main.439e34a1.chunk.js"
+  },
+  {
+    "revision": "657c4d26d01dd64983b3",
+    "url": "/static/js/1.657c4d26.chunk.js"
+  },
+  {
+    "revision": "439e34a1fb57f13a7474",
+    "url": "/static/css/main.ca187a97.chunk.css"
+  },
+  {
+    "revision": "6fa851affc4698f8f3eda8e4e33c5fd2",
+    "url": "/index.html"
+  }
+];
